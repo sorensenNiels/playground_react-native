08ef12d65bab37b79406021475a0cbb1e94c7a75
Niels P. Sorensen <sorensen.niels@gmail.com>
2023-06-27 21:28:58 +0200

    Added redux toolkit and sample

--- src/App.tsx
+++ src/App.tsx
@@ -2,119 +2,126 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
+ * Generated with the Redux TypeScript template
+ * https://github.com/rahsheen/react-native-template-redux-typescript
+ *
  * @format
  */
 
-import {SafeAreaProvider} from 'react-native-safe-area-context';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
+  ScrollView,
+  View,
   Text,
-  useColorScheme,
-  View
+  StatusBar
 } from 'react-native';
 
 import {
+  LearnMoreLinks,
   Colors,
   DebugInstructions,
-  Header,
-  LearnMoreLinks,
   ReloadInstructions
 } from 'react-native/Libraries/NewAppScreen';
+import LearnReduxLinks from './components/LearnReduxLinks';
+import Header from './components/Header';
+import {Counter} from './features/counter/Counter';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+declare const global: {HermesInternal: null | {}};
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black
-          }
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark
-          }
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-const App = (): JSX.Element => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
-  };
-
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
+          style={styles.scrollView}>
           <Header />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white
-            }}>
-            <Section title="Step One">
-              Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-              screen and then come back to see your edits.
-            </Section>
-            <Section title="See Your Changes">
-              <ReloadInstructions />
-            </Section>
-            <Section title="Debug">
-              <DebugInstructions />
-            </Section>
-            <Section title="Learn More">
-              Read the docs to discover what to do next:
-            </Section>
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <Counter />
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.tsx</Text> to change
+                this screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More Redux</Text>
+              <Text style={styles.sectionDescription}>
+                Discover what to do next with Redux:
+              </Text>
+            </View>
+            <LearnReduxLinks />
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More React Native</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
             <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
-    </SafeAreaProvider>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter
+  },
+  engine: {
+    position: 'absolute',
+    right: 0
+  },
+  body: {
+    backgroundColor: Colors.white
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600'
+    fontWeight: '600',
+    color: Colors.black
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400'
+    fontWeight: '400',
+    color: Colors.dark
   },
   highlight: {
     fontWeight: '700'
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right'
   }
 });
 
