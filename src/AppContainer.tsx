c8b39cddbfc213e32bab27171c5b6de0d35f4f74
Niels P. Sorensen <sorensen.niels@gmail.com>
2023-07-13 22:12:39 +0200

    Added important dependecies

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -2,12 +2,15 @@ import App from './App';
 import {store} from './store';
 import {Provider} from 'react-redux';
 import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const AppContainer = () => {
   return (
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <SafeAreaProvider>
+          <App />
+        </SafeAreaProvider>
       </Provider>
     </React.StrictMode>
   );
