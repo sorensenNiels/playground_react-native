d38f928a9c07698177272d9988ab0dfbbb9ebb9f
Niels P. Sorensen <sorensen.niels@gmail.com>
2024-03-26 20:33:31 +0100

    Added notifee

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
+import 'react-native-gesture-handler';
 import {name as appName} from './app.json';
 import AppContainer from './src/AppContainer';
-
-import ReactNativeForegroundService from '@supersami/rn-foreground-service';
-import 'react-native-gesture-handler';
-
-ReactNativeForegroundService.register();
 
 AppRegistry.registerComponent(appName, () => AppContainer);
